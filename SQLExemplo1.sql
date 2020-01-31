fe2c0fb9face36c8e0a7028945ac607bab74b64e
Ana Laura Feltrim <46962757875@info.local>
2020-01-31 12:21:19 -0200

    gdx bt

--- SQLExemplo1.sql
+++ SQLExemplo1.sql
@@ -1,46 +1,83 @@
 create database RoteiroLivros;
 use RoteiroLivros;
 
-create table Generos(
-IdGenero INT PRIMARY KEY IDENTITY NOT NULL,
-Nome VARCHAR (200) NOT NULL
-);
-CREATE TABLE Autor(
-IdAutor INT PRIMARY KEY IDENTITY NOT NULL,
-Nome VARCHAR (200)
-);
-CREATE TABLE Livro(
-IdLivro INT PRIMARY KEY IDENTITY,
-Titulo VARCHAR (200),
-IdGenero INT FOREIGN KEY REFERENCES Generos (IdGenero),
-IdAutor INT FOREIGN KEY REFERENCES Autor(IdAutor)
-);
+	create table Generos(
+	IdGenero INT PRIMARY KEY IDENTITY NOT NULL,
+	Nome VARCHAR (200) NOT NULL
+	);
+	CREATE TABLE Autor(
+	IdAutor INT PRIMARY KEY IDENTITY NOT NULL,
+	Nome VARCHAR (200)
+	);
+	CREATE TABLE Livro(
+	IdLivro INT PRIMARY KEY IDENTITY,
+	Titulo VARCHAR (200),
+	IdGenero INT FOREIGN KEY REFERENCES Generos (IdGenero),
+	IdAutor INT FOREIGN KEY REFERENCES Autor(IdAutor)
+	);
  
 
 
- SELECT*FROM Autor;
- SELECT*FROM Generos;
- SELECT*FROM Livro;
+	 SELECT*FROM Autor;
+	 SELECT*FROM Generos;
+	 SELECT*FROM Livro;
 
- insert into Autor (Nome)
- values ('George Orwell'),
-	    ('J.R.R. Tolkien'),
-		('Stephen King'),
-		('Percy Jackson'),
-		('George Lucas');
+	 insert into Autor (Nome)
+	 values ('George Orwell'),
+			('J.R.R. Tolkien'),
+			('Stephen King'),
+			('Percy Jackson'),
+			('George Lucas');
 
-insert into Generos (Nome)
-values ('Suspense'),
-	   ('Aventura'),
-	   ('Terror'),
-	   ('Ficçao'),
-	   ('Mitologia');
+	insert into Generos (Nome)
+	values ('Suspense'),
+		   ('Aventura'),
+		   ('Terror'),
+		   ('Ficçao'),
+		   ('Mitologia');
 
-insert into Livro (Titulo, IdGenero, IdAutor)
-values('Ladrão de Raios',5,4),
-	  ('Revolução dos Bichos',4,1),
-	  ('IT: A coisa',3,3),
-	  ('Senhor  dos Anéis',2,2),
-	  ('Star Wars', 4,5);
+	insert into Livro (Titulo, IdGenero, IdAutor)
+	values('Ladrão de Raios',5,4),
+		  ('Revolução dos Bichos',4,1),
+		  ('IT: A coisa',3,3),
+		  ('Senhor  dos Anéis',2,2),
+		  ('Star Wars', 4,5);
+
+		  update Autor 
+		  set Nome = 'Rick Rioradan'
+		  where IdAutor = 4;
+
+		  update Livro 
+		  set Titulo = 'Percy Jackson'
+		  where IdLivro = 1;
+
+		  delete from Livro
+		  where IdLivro = 5;
+
+		  delete from Autor
+		  where IdAutor = 5;
  
- 
+		  delete from Livro
+		  where IdAutor in (2,3);
+
+		  update Livro
+		  set Titulo = 'livro bom' 
+		  where IdLivro in (1,2);
+
+		  update Livro
+		  set Titulo = 'Percy Jackson''Senhor dos Aneis'
+		  where IdLivro in (1,2);
+
+		  update Livro
+		  set Titulo = 'Senhor dos Anéis'
+		  where IdLivro = 2;
+
+		  select*from Autor;
+		  select*from Generos;
+		  select*from Livro;
+
+		  select IdLivro, Titulo ,IdAutor from Livro;
+
+		  select IdLivro, Titulo, IdGenero from Livro;
+
+	  
--- SQLOptus-DQL.sql
+++ SQLOptus-DQL.sql
@@ -78,7 +78,84 @@ use Optus;
 	values ('Scrawny', 090801 , 25 , 97, 1, 1 );
 
 	insert into Albuns (Nome, Lancamento, Visualizacoes, Minutos, IdArtista, IdEstilosMusicais)
-    values ('Melophobia', 070809, 97, 90, 2, 2),
-	       ('Poste', 050197, 123456, 67,3,3),
-		   ('Feijão', 12345, 676676, 160,4,4),
-		   ('Carnaval',010101, 12345, 4320,5,5);
+    values ('Melophobia', '29-08-2001', 97, 90, 2, 2),
+	       ('Poste', '08-09-1978' , 123456, 67,3,3),
+		   ('Feijão', '02-05-2019', 676676, 160,4,4),
+		   ('Carnaval', '23-02-2020', 12345, 4320,5,5);
+
+	
+	update Usuario
+	set IdTipoUsuario = '1'
+	where IdUsuario = 2;
+
+	update Albuns 
+	set Visualizacoes = 234
+	where IdAlbum = 2;
+
+	delete from Albuns
+	where IdAlbum = 5;
+
+	select Nome, Lancamento from Albuns
+	where (Visualizacoes > 1000000) or (Minutos > 100000000);
+
+	select IdArtista, Nome from Albuns 
+	where Nome like 'Scrawny';
+	
+	select IdTipoUsuario, Nome from Usuario
+	where IdTipoUsuario =2 ;
+
+	update Usuario
+	set IdTipoUsuario = 2
+	where IdUsuario in (2,4);
+
+	select Nome, Visualizacoes from Albuns
+	order by Visualizacoes desc;
+
+	select count (IdUsuario) from Usuario;
+
+	select IdArtista, Nome, Lancamento, Minutos from Albuns
+	where IdArtista = 2;
+	
+	select Lancamento, Nome from Albuns
+	where Lancamento = 12345;
+
+	select IdEstilosMusicais, IdArtista from Albuns
+	where IdEstilosMusicais = 4;
+	select * from Estilos;
+	Select * from Artistas;
+
+	select Nome, IdArtista, Lancamento from Albuns order by Lancamento;
+	select * from Artistas;
+
+	alter table Albuns 
+	drop column Lancamento;
+
+	alter table Albuns 
+	add Lancamento date ;
+
+	update Albuns 
+	set Lancamento = '29-08-2001'
+	where IdAlbum = 1;
+
+	update Albuns 
+	set Lancamento = '09-08-1987'
+	where IdAlbum = 2;
+	
+	update Albuns 
+	set Lancamento = '29-05-2019'
+	where IdAlbum = 3;
+	
+	update Albuns 
+	set Lancamento = '23-02-2020'
+	where IdAlbum = 1;
+
+	update Albuns 
+	set Lancamento = '29-08-2001'
+	where IdAlbum = 1;
+	
+	update Albuns 
+	set Lancamento = '23-02-2020'
+	where IdAlbum = 4;
+
+
+	
